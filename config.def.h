88c89af74969e9ed0a1fdcee65420455947ef8d2
gurjal <shaanraya135@gmail.com>
2021-01-21 14:32:53 -0800

    basic setup

--- config.def.h
+++ config.def.h
@@ -65,5 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ cpu_perc, "cpu: %s% / ", "" },
+	{ ram_perc, "mem: %s% / ", "" },
+	{ run_command, "vol: %s% / ", "echo $(pamixer --get-volume)" },
+	{ datetime, "%s",           "%m.%d.%C / %I:%M %p" },
 };
