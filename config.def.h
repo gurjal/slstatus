a9f28671b423cc6982e074117a1389becf44d6ba
gurjal <shaanraya135@gmail.com>
2022-07-29 11:35:24 -0700

    set new default layout

--- config.def.h
+++ config.def.h
@@ -66,12 +66,12 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s",           "%I:%M %p ( %Y.%d.%m ( " }, // 12hr clock
-	{ datetime, "\\t %s ","%H:%M" }, // 24hr clock
-	{ cpu_perc, "\\c %s ", "" },
-	{ ram_perc, "\\r %s ", "" },
-	{ run_command, "\\v %s ", "printf \%02d $(pulsemixer --get-volume | cut -d' ' -f1)" },
-	{ battery_perc, "\\b %s" , "BAT0"},
-	{ battery_state, "%s " , "BAT0"},
-	{ datetime, "\\d %s ", "%a %m.%d" }, // 24hr clock
-	{ wifi_essid, "\\n %s", "wlp0s20f3" },
+	/* { datetime, " %s ","" }, // 24hr clock */
+	{ cpu_perc, "c%s", "" },
+	{ ram_perc, " r%s", "" },
+	{ battery_perc, " / b%s" , "BAT0"},
+	{ battery_state, "%s" , "BAT0"},
+	{ run_command, " / v%s", "printf \%02d $(pulsemixer --get-volume | cut -d' ' -f1)" },
+	{ datetime, " / %s", "%a %m.%d %H:%M" }, // 24hr clock
+	{ wifi_essid, " / %s ", "wlp0s20f3" },
 };
